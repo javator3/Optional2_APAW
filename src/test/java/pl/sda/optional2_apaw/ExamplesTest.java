7ad8c4b2b44cdf9c777487d6e9a0423dc321d4f9
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-03-03 09:41:17 +0100

    optional test passed

--- src/test/java/pl/sda/optional2_apaw/ExamplesTest.java
+++ src/test/java/pl/sda/optional2_apaw/ExamplesTest.java
@@ -9,7 +9,7 @@ public class ExamplesTest {
 
 
     @Test
-    public void whenCreateEmptyOptionalReturnTrue (){
+    public void whenCreateEmptyOptionalReturnTrue() {
 
         Optional<String> empty = Optional.empty();
         assertFalse(empty.isPresent());
@@ -17,16 +17,50 @@ public class ExamplesTest {
     }
 
     @Test
-    public void givenNonNull (){
+    public void givenNonNull() {
         String name = "testowa wartość";
         Optional<String> opt = Optional.of(name);
         assertEquals("Optional[testowa wartość]", opt.toString());
     }
 
     @Test(expected = NullPointerException.class)
-    public void givenNullWhenErrorOnCreate (){
+    public void givenNullWhenErrorOnCreate() {
         String name = null;
         Optional<String> opt = Optional.of(name);
     }
 
+    @Test
+    public void givenNullWhenCreateNullable() {
+        String name = null;
+        Optional<String> opt = Optional.ofNullable(name);
+        assertEquals("Optional.empty", opt.toString());
+
+    }
+
+    @Test
+    public void givenOptionalWhenIfPresentOkThenCorrect() {
+
+        Optional<String> name = Optional.ofNullable(null);
+
+        name.ifPresent(n -> System.out.println("Witaj: " + n));
+
+    }
+
+    @Test
+    public void whenOrElseWorksThenCorrect() {
+        String name = null;
+        String nameGood = Optional.ofNullable(name).orElse("Nowe Imie");
+
+        Optional<String> opt = Optional.of("Wartość");
+        opt.get();
+        System.out.println(opt);
+        assertEquals("Nowe Imie", nameGood);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenOrElseWorksThenCorrect1() {
+        String name = null;
+        String nameGood = Optional.ofNullable(name).orElseThrow(() -> new IllegalArgumentException());
+
+    }
 }
